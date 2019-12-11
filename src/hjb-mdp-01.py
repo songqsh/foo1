7b9f31b0d3b99f0f5d5500107b4655ccf68419ba
Kennith <songqsh@gmail.com>
2019-12-10 21:32:37 -0500

    Update hjb-mdp-01.py

--- src/hjb-mdp-01.py
+++ src/hjb-mdp-01.py
@@ -14,22 +14,26 @@ class MDP:
     def __init__(
             self, 
             dim = 2,
+            mesh_n = 20, #better to be even number
+            lam = 0.0,
             verbose = True
             ):
         self.dim = dim
+        self.h = 2.0/mesh_n
+        self.rate = dim/(dim+self.h**2*lam)
         if verbose:
             print(str(dim)+
                   '-d MDP from HJB'
                   )
             
     #define reflecting states
-    #input: n-d array for a state
+    #input: d-array for a state
     #return: true/false
     def is_reflecting(self, state):
         return False
     
     #define absorbing states
-    #input: n-d array for a state
+    #input: d-array for a state
     #return: true/false
     def is_absorbing(self, state):
         if state.shape != (self.dim, 1):
@@ -42,11 +46,28 @@ class MDP:
         
     #define one step move
     #input:
-    #   n-d array for state
-    #   n-d array for action
+    #   d-array for state
+    #   d-array for action
     #return: 
-    #   n-d array for state after one step 
-    #   a float number for reward
+    #   d*2d-array for possible states after one step
+    #   1*d-array for their transition probability
+    #   float for instant reward
+    
+    def step(self, state, action):
+        del_state = np.append(np.eye(self.dim), - np.eye(self.dim), axis =1)*self.h
+        if self.is_absorbing(state):
+            next_state = state
+        else:
+            next_state = state+del_state
+            
+        trans_prob = np.append(1+self.h*action, 1-self.h*action).reshape(1, 2*self.dim)
+        
+        ell = lambda x, a: self.dim+2*np.sum(x**2)+ np.sum(a**2)*0.5
+        reward = self.h**2*ell(state, action)/self.dim
+            
+        return next_state, trans_prob, reward
+    
+
     
         
         
